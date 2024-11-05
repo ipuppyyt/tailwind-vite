99a7e763651d250f478762d62e25eb02fac14d06
iPuppyYT <dhanush6102002@gmail.com>
2024-11-05 10:05:41 +0530

    cli error handling fix

--- cli/index.mjs
+++ cli/index.mjs
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-const prompts = require('prompts');
-const fs = require('fs');
-const path = require('path');
+import prompts from 'prompts';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join, resolve } from 'path';
+import ora from 'ora';
 
-const handleExit = (error, isTest) => {
-    if (error) {
-        console.error(`\n\x1b[31m❌ ${error.message}\x1b[0m`);
-    }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const handleExit = (error) => {
+    error && console.error(`\n\x1b[31m❌ ${error.message}\x1b[0m`);
     console.log(`\n\x1b[33m🚪 Exiting. Goodbye! ✌️\x1b[0m`);
-    if (!isTest) process.exit(0);
-    throw new Error("Exit called");
+    process.exit(0);
 };
-
-process.on('SIGINT', handleExit);
 
 async function main() {
     try {
@@ -24,7 +24,10 @@ async function main() {
                 message: '📝 Project name:',
                 initial: 'vite-tailwind-app',
                 validate: (value) => value.trim() === "" ? 'Project name is required.' : true,
-                format: (value) => value.trim()
+                format: (value) => value.trim(),
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             },
             {
                 type: 'select',
@@ -32,33 +35,17 @@ async function main() {
                 message: '📚 Select a library or framework:',
                 choices: [
                     { title: '\x1b[33mVanilla\x1b[0m 🍦', value: 'vanilla' },
-                    // { title: '\x1b[31mVue\x1b[0m 🍇', value: 'vue' },
                     { title: '\x1b[34mReact\x1b[0m 💙', value: 'react' },
-                    // { title: '\x1b[35mPreact\x1b[0m 🍑', value: 'preact' },
-                    // { title: '\x1b[36mLit\x1b[0m 📜', value: 'lit' },
-                    // { title: '\x1b[32mSvelte\x1b[0m 🦌', value: 'svelte' },
-                    // { title: '\x1b[33mSolid\x1b[0m 🧱', value: 'solid' },
-                    // { title: '\x1b[31mQwik\x1b[0m 🚀', value: 'qwik' },
-                    // { title: '\x1b[35mAngular\x1b[0m 🅰️', value: 'angular' },
-                    // { title: '\x1b[36mOthers\x1b[0m 🛠️', value: 'others' }
                 ],
                 initial: 0,
-                validate: (value) => value.trim() === "" ? 'Library is required.' : true
+                validate: (value) => value.trim() === "" ? 'Library is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             }
         ]);
 
         const { projectName, library } = response;
-
-        if (!projectName) {
-            console.error(`\x1b[31m❌ Project name is required.\x1b[0m`);
-            handleExit(null);
-        }
-
-        if (!library) {
-            console.error(`\x1b[31m❌ Library is required.\x1b[0m`);
-            console.log(`\x1b[33m🚪 Exiting. Goodbye! ✌️\x1b[0m`);
-            process.exit(0);
-        }
 
         let variantResponse;
         if (library === 'vanilla') {
@@ -70,7 +57,11 @@ async function main() {
                     { title: '\x1b[36mTypeScript\x1b[0m 📘', value: 'ts' },
                     { title: '\x1b[32mJavaScript\x1b[0m ✨', value: 'js' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },                
             });
         } else if (library === 'vue') {
             variantResponse = await prompts({
@@ -83,7 +74,11 @@ async function main() {
                     { title: '\x1b[33mCustomise with create-vue\x1b[0m 🎨', value: 'create-vue' },
                     { title: '\x1b[33mNuxt.js\x1b[0m 📦', value: 'nuxt' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'react') {
             variantResponse = await prompts({
@@ -97,7 +92,11 @@ async function main() {
                     { title: '\x1b[35mTypeScript + SWC\x1b[0m 🔥', value: 'ts_swc' },
                     { title: '\x1b[34mRemix\x1b[0m 🚄', value: 'remix' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'preact') {
             variantResponse = await prompts({
@@ -109,7 +108,11 @@ async function main() {
                     { title: '\x1b[32mJavaScript\x1b[0m ✨', value: 'js' },
                     { title: '\x1b[33mCustomise with create-preact\x1b[0m 🎨', value: 'create-preact' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'lit') {
             variantResponse = await prompts({
@@ -120,7 +123,11 @@ async function main() {
                     { title: '\x1b[36mTypeScript\x1b[0m 📘', value: 'ts' },
                     { title: '\x1b[32mJavaScript\x1b[0m ✨', value: 'js' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'svelte') {
             variantResponse = await prompts({
@@ -132,7 +139,11 @@ async function main() {
                     { title: '\x1b[32mJavaScript\x1b[0m ✨', value: 'js' },
                     { title: '\x1b[33mSvelteKit\x1b[0m 🚀', value: 'sveltekit' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'solid') {
             variantResponse = await prompts({
@@ -143,7 +154,11 @@ async function main() {
                     { title: '\x1b[36mTypeScript\x1b[0m 📘', value: 'ts' },
                     { title: '\x1b[32mJavaScript\x1b[0m ✨', value: 'js' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'qwik') {
             variantResponse = await prompts({
@@ -155,7 +170,11 @@ async function main() {
                     { title: '\x1b[32mJavaScript\x1b[0m ✨', value: 'js' },
                     { title: '\x1b[33mQwikCity\x1b[0m 🏙️', value: 'qwikcity' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'angular') {
             variantResponse = await prompts({
@@ -166,7 +185,11 @@ async function main() {
                     { title: '\x1b[31mAngular\x1b[0m 🅰️', value: 'angular' },
                     { title: '\x1b[36mAnalog\x1b[0m 📟', value: 'analog' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else if (library === 'others') {
             variantResponse = await prompts({
@@ -177,7 +200,11 @@ async function main() {
                     { title: '\x1b[33mcreate-vite-extra\x1b[0m 🎨', value: 'create-vite-extra' },
                     { title: '\x1b[34mcreate-electron-vite\x1b[0m 🎨', value: 'create-electron-vite' }
                 ],
-                initial: 0
+                initial: 0,
+                validate: (value) => value.trim() === '' ? 'Variant is required.' : true,
+                onState: (state) => {
+                    state.aborted && handleExit(null);
+                },
             });
         } else {
             console.error(`\x1b[31m❌ Invalid library.\x1b[0m`);
@@ -187,12 +214,6 @@ async function main() {
 
         const { variant } = variantResponse;
 
-        if (!variant) {
-            console.error(`\x1b[31m❌ Variation is required.\x1b[0m`);
-            console.log(`\x1b[33m🚪 Exiting. Goodbye! ✌️\x1b[0m`);
-            process.exit(0);
-        }
-
         const styleResponse = await prompts({
             type: 'select',
             name: 'style',
@@ -201,19 +222,17 @@ async function main() {
                 { title: '\x1b[32mCSS\x1b[0m 🖌️', value: 'css' },
                 { title: '\x1b[35mSCSS\x1b[0m 🎨', value: 'scss' }
             ],
-            initial: 0
+            initial: 0,
+            validate: (value) => value.trim() === '' ? 'Stylesheet format is required.' : true,
+            onState: (state) => {
+                state.aborted && handleExit(null);
+            },
         });
 
         const { style } = styleResponse;
 
-        if (!style) {
-            console.error(`\x1b[31m❌ Stylesheet format is required.\x1b[0m`);
-            console.log(`\x1b[33m🚪 Exiting. Goodbye! ✌️\x1b[0m`)
-            process.exit(0);
-        }
-
-        const templatePath = path.join(__dirname, '../templates', library, variant, style);
-        const projectPath = projectName === "." ? process.cwd() : path.resolve(process.cwd(), projectName);
+        const templatePath = join(__dirname, '../templates', library, variant, style);
+        const projectPath = projectName === "." ? process.cwd() : resolve(process.cwd(), projectName);
 
         if (!fs.existsSync(projectPath)) {
             fs.mkdirSync(projectPath, { recursive: true });
@@ -230,33 +249,40 @@ async function main() {
                         { title: '\x1b[31m🚪 Exit\x1b[0m', value: 'exit' }
                     ],
                     initial: 0,
-                    validate: (value) => value.trim() === '' ? 'Please select an option' : true
+                    validate: (value) => value.trim() === '' ? 'Please select an option' : true,
+                    onState: (state) => {
+                        state.aborted && handleExit(null);
+                    },
                 });
 
                 if (dirprompt.value === 'exit') {
-                    return handleExit(null, isTest);
+                    return handleExit(null);
                 } else if (dirprompt.value === 'clear') {
                     files.forEach((file) => {
-                        fs.rmSync(path.join(projectPath, file), { recursive: true });
+                        fs.rmSync(join(projectPath, file), { recursive: true });
                     });
                     console.log(`\x1b[33m🗑️ Directory cleared.\x1b[0m`);
                 } else if (dirprompt.value === 'ignore') {
                     console.log('\x1b[33m🚧 Ignoring existing files and continuing...\x1b[0m');
                 } else {
                     console.error(`\x1b[31m❌ Invalid option.\x1b[0m`);
-                    return handleExit(null, isTest);
+                    return handleExit(null);
                 }
             }
         }
 
         if (!fs.existsSync(templatePath)) {
             console.error(`\x1b[31m❌ Template does not exist.\x1b[0m`);
-            process.exit(1);
+            handleExit(null);
+            process.exit(0);
         }
+
+        const spinner = ora('Setting up your project...').start();
 
         fs.cpSync(templatePath, projectPath, { recursive: true });
 
-        console.log(`\n\x1b[32m🚀 Setting up your project...\x1b[0m`);
+        spinner.succeed('Project setup completed!');
+
         console.log(`\n\x1b[32m✔️  ${projectName} is ready! To get started:\x1b[0m`);
         if (projectName !== ".") {
             console.log(`\n  cd ${projectName}`);
@@ -269,5 +295,3 @@ async function main() {
 }
 
 main();
-
-module.exports = { main };
